3a85254ee99f6aa9d805d47303b6e1a18c6c2501
MrKTPPPP <87939752+MrKTPPPP@users.noreply.github.com>
2023-02-07 19:56:12 +0700

    Last test push

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,7 +82,7 @@ const IndexPage = () => (
         style={{ marginBottom: `var(--space-3)` }}
       />
       <h1>
-        <b>Chayapol!</b>
+        <b>MRKTPPPPPPP!!!</b>
       </h1>
       <p className={styles.intro}>
         <b>Example pages:</b>{" "}
